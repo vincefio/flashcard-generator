62e724dfc8db334a72163ce47d046d869347bbfb
Vince <vincentfiorilli@gmail.com>
2018-07-05 19:58:15 -0400

    before inquirer constructors working

--- ClozeCard.js
+++ ClozeCard.js
@@ -5,10 +5,32 @@ var ClozeCard = function(text, cloze){
 
 //create prototype for this.partial which returns ... instead of cloze
 ClozeCard.prototype.partial = function(){
+  let partialText
+  /*
   let splitText = this.text.split(" ")
   let splitCloze = this.cloze.split(" ")
-  console.log(splitText + " " + splitCloze)
-  //this.partial =
+  */
+//  console.log(splitText + " " + splitCloze)
+
+  if(this.text.indexOf(this.cloze) != -1){
+    /*console.log('string is inside string')
+    let startIndex = this.text.indexOf(this.cloze)
+    console.log(startIndex)
+    let endIndex = startIndex + this.cloze.length
+    console.log('endIndex ' + endIndex)*/
+    partialText = this.text.split(this.cloze)
+    //console.log(this.text)
+  }
+  else{
+    console.log('Answer must be present in the question')
+    return
+  }
+
+  partialText = String(partialText[1])
+  return partialText
+  //console.log(partialText)
+  //console.log(this.text + ' ' + typeof partialText)
+  //console.log(typeof partialText)
 }
 
 module.exports = ClozeCard
--- app.js
+++ app.js
@@ -17,8 +17,10 @@ basicCardArr.push(firstPresident, woodStock, blackSabbath, ledZeppelin)
 
 var firstPresidentCloze = new ClozeCard("George Washington was the first president of the United States.", "George Washington");
 //console.log(firstPresidentCloze.cloze);
-var woodStockCloze = new ClozeCard('Jimi Hendrix played the famouse version of "The Star Spangled Banner at Woodstock', 'Jimi Hendrix')
+var woodStockCloze = new ClozeCard('Jimi Hendrix played the famouse version of "The Star Spangled Banner at Woodstock"', 'Jimi Hendrix')
 var blackSabbathCloze = new ClozeCard('Black Sabbath is the title of the first track on Black Sabbath\'s self titled album', 'Black Sabbath')
 var ledZeppelinCloze = new ClozeCard('Led Zeppelin\'s debut album was released in 1968', 1968)
 clozeCardArr.push(firstPresidentCloze, woodStockCloze, blackSabbathCloze, ledZeppelinCloze)
-//firstPresidentCloze.partial()
+let x = woodStockCloze.partial()
+//console.log(typeof x)
+console.log(x)
