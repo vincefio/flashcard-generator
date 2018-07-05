3e652cb0be08eaa8e0fb99b8cd81a08bec3af78f
Vince <vincentfiorilli@gmail.com>
2018-07-05 17:24:51 -0400

    prototype working

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,7 +1,14 @@
 var ClozeCard = function(text, cloze){
   this.cloze = cloze;//cloze is the same as the back on basic card, also the answer
   this.text = text;
+}
 
+//create prototype for this.partial which returns ... instead of cloze
+ClozeCard.prototype.partial = function(){
+  let splitText = this.text.split(" ")
+  let splitCloze = this.cloze.split(" ")
+  console.log(splitText + " " + splitCloze)
+  //this.partial =
 }
 
 module.exports = ClozeCard
--- app.js
+++ app.js
@@ -11,10 +11,14 @@ var firstPresident = new BasicCard("Who was the first president of the United St
 var woodStock = new BasicCard('Who played the famouse version of "The Star Spangled Banner at Woodstock?"', 'Jimi Hendrix')
 var blackSabbath = new BasicCard('What is the title of the first track on Black Sabbath\'s self titled album', 'Black Sabbath')
 var ledZeppelin = new BasicCard('In what year was Led Zeppelin\'s debut album released', '1968')
+//push all these objects into the basicCardArr
+basicCardArr.push(firstPresident, woodStock, blackSabbath, ledZeppelin)
 
-console.log(blackSabbath.front);
+
 var firstPresidentCloze = new ClozeCard("George Washington was the first president of the United States.", "George Washington");
 //console.log(firstPresidentCloze.cloze);
 var woodStockCloze = new ClozeCard('Jimi Hendrix played the famouse version of "The Star Spangled Banner at Woodstock', 'Jimi Hendrix')
 var blackSabbathCloze = new ClozeCard('Black Sabbath is the title of the first track on Black Sabbath\'s self titled album', 'Black Sabbath')
 var ledZeppelinCloze = new ClozeCard('Led Zeppelin\'s debut album was released in 1968', 1968)
+clozeCardArr.push(firstPresidentCloze, woodStockCloze, blackSabbathCloze, ledZeppelinCloze)
+//firstPresidentCloze.partial()
